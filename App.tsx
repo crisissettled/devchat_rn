9225fe5698d18387d6e775abdafd884074c1d2fa
James Liu <jliu@sirokygroup.com>
2024-01-13 16:52:05 -0500

    drawer screen

--- App.tsx
+++ App.tsx
@@ -1,58 +1,19 @@
-import {Button, alert} from 'react-native';
+import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
-import {RootStackParamList} from './types/StackScreen';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
 import HomeScreen from './screens/HomeScreen';
-import CreatePostScreen from './screens/CreatePostScreen';
+import NotificationsScreen from './screens/NotificationsScreen';
 
-import LogoTitle from './component/LogoTitle';
-
-const Stack = createNativeStackNavigator<RootStackParamList>();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: '#f2c1d0',
-          },
-          headerTintColor: '#e5771d',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            // title: 'My home',
-            // headerStyle: {
-            //   backgroundColor: '#baa',
-            // },
-            // headerTintColor: '#dfd',
-            // headerTitleStyle: {
-            //   fontWeight: 'bold',
-            // },
-
-            headerTitle: props => <LogoTitle />,
-            headerRight: () => (
-              <Button
-                onPress={() => console.log('This is a Header button!')}
-                title="Info"
-                color="#0081F1"
-              />
-            ),
-          }}
-        />
-        <Stack.Screen
-          name="CreatePost"
-          component={CreatePostScreen}
-          options={({route}) => ({title: route.params.name})}
-        />
-      </Stack.Navigator>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,38 +1,12 @@
-import {useState, useEffect} from 'react';
-import {View, Text, Button, TextInput} from 'react-native';
+import {Button, View} from 'react-native';
 
-import {PropsHome} from '../types/StackScreen';
-
-function HomeScreen({navigation, route}: PropsHome) {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    if (route.params?.post) {
-      console.log(route.params?.post, 'post1');
-    }
-  }, [route.params?.post]);
-
-  useEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <Button onPress={() => setCount(c => c + 1)} title="Update count" />
-      ),
-    });
-  }, [navigation]);
-
+function HomeScreen({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Count: {count}</Text>
       <Button
-        title="Create post"
-        onPress={() =>
-          navigation.navigate('CreatePost', {
-            userId: 'james',
-            name: 'my create post',
-          })
-        }
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
       />
-      <Text style={{margin: 10}}>Post: {route.params?.post}</Text>
     </View>
   );
 }
