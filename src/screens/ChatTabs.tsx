afe0a824f46bbca9d93d65723d461918b4fbbd6f
James Liu <jliu@sirokygroup.com>
2024-01-30 08:42:52 -0500

    logout prompt

--- src/screens/ChatTabs.tsx
+++ src/screens/ChatTabs.tsx
@@ -1,3 +1,5 @@
+import {useEffect} from 'react';
+import {Alert, BackHandler} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import {TabParamList} from '@shared/types/navigationTypes';
@@ -8,6 +10,29 @@ import ProfileScreen from '@screens/chat/ProfileScreen';
 const Tab = createBottomTabNavigator<TabParamList>();
 
 function ChatTabsScreen() {
+  useEffect(() => {
+    const backAction = () => {
+      // Prompt the user before leaving the screen
+      Alert.alert('LOGOUT', 'are you going to log out?', [
+        {text: 'Cancel', style: 'cancel', onPress: () => null},
+        {
+          text: 'Log out',
+          style: 'destructive',
+          onPress: () => BackHandler.exitApp(),
+        },
+      ]);
+
+      return true;
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      backAction,
+    );
+
+    return () => backHandler.remove();
+  }, []);
+
   return (
     <Tab.Navigator>
       <Tab.Screen name="Chat" component={ChatScreen} />
